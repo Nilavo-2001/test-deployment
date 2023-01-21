ac41a8de0cea9bc3ba1a9bc3fc7ddf2598e58184
Nilavo <iem.nilavo2001@gmail.com>
2023-01-22 02:24:50 +0530

    contact removed

--- index.js
+++ index.js
@@ -28,38 +28,38 @@ app.get("/", (req, res) => {
     return res.send("<h1> Testing  </h1>")
   });
 });
-app.get("/delete", (req, res) => {
-  console.log(req.query.id);
-  let id = req.query.id;
-  Contact.findByIdAndDelete(id, (err) => {
-    if (err) {
-      console.log("Something went wrong");
-      return;
-    }
-    res.redirect("back");
-  });
-  // let reqIndex = contact_list.findIndex((contact) => {
-  //   return contact.Phone == req.query.phone;
-  // });
-  // contact_list.splice(reqIndex, 1);
-});
-app.post("/sendData", (req, res) => {
-  //contact_list.push(req.body);
-  Contact.create(
-    {
-      Name: req.body.Name,
-      Phone: req.body.Phone,
-    },
-    (err, newcontact) => {
-      if (err) {
-        console.log("Something went wrong while creating data");
-        return;
-      }
-      console.log(newcontact._id);
-      return res.redirect("back");
-    }
-  );
-});
+// app.get("/delete", (req, res) => {
+//   console.log(req.query.id);
+//   let id = req.query.id;
+//   Contact.findByIdAndDelete(id, (err) => {
+//     if (err) {
+//       console.log("Something went wrong");
+//       return;
+//     }
+//     res.redirect("back");
+//   });
+//   // let reqIndex = contact_list.findIndex((contact) => {
+//   //   return contact.Phone == req.query.phone;
+//   // });
+//   // contact_list.splice(reqIndex, 1);
+// });
+// app.post("/sendData", (req, res) => {
+//   //contact_list.push(req.body);
+//   Contact.create(
+//     {
+//       Name: req.body.Name,
+//       Phone: req.body.Phone,
+//     },
+//     (err, newcontact) => {
+//       if (err) {
+//         console.log("Something went wrong while creating data");
+//         return;
+//       }
+//       console.log(newcontact._id);
+//       return res.redirect("back");
+//     }
+//   );
+// });
 app.listen(port, (err) => {
   if (err) {
     console.log("Something went wrong", err);
