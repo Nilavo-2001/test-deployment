f6a709a0a58dee9d8843688fe8cc4f49b3eba06a
Nilavo <iem.nilavo2001@gmail.com>
2023-01-22 02:44:29 +0530

    undo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/contact_list_db");
+mongoose.connect("mongodb://127.0.0.1:27017/contact_list_db");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error occured"));
 db.once("open", () => {
